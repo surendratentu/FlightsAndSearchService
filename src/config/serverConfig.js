ccbfbcc5655eb8760d1d4edc71f71069bbc8d30f
Surendratentu <164079011+Surendratentu@users.noreply.github.com>
2025-10-25 12:06:10 +0530

    Setup basic bodyparser

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 module.exports = {
     PORT : process.env.PORT
