488eb333d92ac3390a5a90572caa82f229df626a
Surendratentu <164079011+Surendratentu@users.noreply.github.com>
2025-10-25 13:45:47 +0530

    Completed basic sequelize setup for create a db

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,10 +1,8 @@
-const bodyParser = require('body-parser');
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
 
 module.exports = {
     PORT : process.env.PORT
